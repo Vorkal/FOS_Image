7ba29295c04e53fd07bc69ddcf85623ac00ba50a
Nicolas Everhart <mail@nicolaseverhart.com>
2025-09-26 12:48:59 -0400

    Switched to a basic build

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-set -ouex pipefail
 
-### Install packages
-
-# this installs a package from fedora repos
-dnf5 install -y tmux neovim
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
